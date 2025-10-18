953fab3d080c4a69dfa3cf55d77aaa8b10565fe8
Aidas Bendoraitis <aidasbend@yahoo.com>
2025-10-18 02:19:45 +0200

    🔨 Room inclusion into templates refactored - not creating or loading them in the view, but rather the template.

--- demo_project/apps/events/views.py
+++ demo_project/apps/events/views.py
@@ -1,18 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.contrib.contenttypes.models import ContentType
-from django.http import JsonResponse
-from django.views.decorators.http import require_POST
 
 from .models import Event
-from django_messaging.models import ChatRoom, ChatMembership
 
 
 @login_required
 def event_list(request):
     """Display a list of events"""
     events = Event.objects.filter(is_public=True)
-    return render(request, 'events/event_list.html', {'events': events})
+    return render(request, "events/event_list.html", {"events": events})
 
 
 @login_required
@@ -20,33 +16,7 @@ def event_detail(request, pk):
     """Display event detail with chat room"""
     event = get_object_or_404(Event, pk=pk, is_public=True)
 
-    # Get or create chat room for this event
-    content_type = ContentType.objects.get_for_model(Event)
-    room, created = ChatRoom.objects.get_or_create(
-        content_type=content_type,
-        object_id=event.pk,
-        defaults={
-            'name': f"Event Chat: {event.title}",
-            'is_room': True,
-            'is_group': True,
-        }
-    )
-
-    # Check if user is a member of the chat room
-    is_member = room.members.filter(id=request.user.id).exists()
-
-    # Import messaging settings for room template
-    from django_messaging.conf import app_settings
-
     context = {
-        'event': event,
-        'room': room,
-        'is_member': is_member,
-        'CONTENT_TOP_OFFSET': app_settings.CONTENT_TOP_OFFSET,
-        'CONTENT_BOTTOM_OFFSET': app_settings.CONTENT_BOTTOM_OFFSET,
-        'ENABLE_CHAT_ROOMS': app_settings.ENABLE_CHAT_ROOMS,
+        "event": event,
     }
-    return render(request, 'events/event_detail.html', context)
-
-
-
+    return render(request, "events/event_detail.html", context)
--- demo_project/apps/videos/views.py
+++ demo_project/apps/videos/views.py
@@ -1,20 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.contrib.contenttypes.models import ContentType
-from django.http import JsonResponse
-from django.views.decorators.http import require_POST
-from django.views.decorators.csrf import csrf_exempt
-import json
 
 from .models import Video
-from django_messaging.models import ChatRoom, ChatMembership
 
 
 @login_required
 def video_list(request):
     """Display a list of videos"""
     videos = Video.objects.filter(is_public=True)
-    return render(request, 'videos/video_list.html', {'videos': videos})
+    return render(request, "videos/video_list.html", {"videos": videos})
 
 
 @login_required
@@ -22,33 +16,7 @@ def video_detail(request, pk):
     """Display video detail with chat room"""
     video = get_object_or_404(Video, pk=pk, is_public=True)
 
-    # Get or create chat room for this video
-    content_type = ContentType.objects.get_for_model(Video)
-    room, created = ChatRoom.objects.get_or_create(
-        content_type=content_type,
-        object_id=video.pk,
-        defaults={
-            'name': f"Discussion: {video.title}",
-            'is_room': True,
-            'is_group': True,
-        }
-    )
-
-    # Check if user is a member
-    is_member = room.members.filter(id=request.user.id).exists()
-
-    # Import messaging settings for room template
-    from django_messaging.conf import app_settings
-
     context = {
-        'video': video,
-        'room': room,
-        'is_member': is_member,
-        'CONTENT_TOP_OFFSET': app_settings.CONTENT_TOP_OFFSET,
-        'CONTENT_BOTTOM_OFFSET': app_settings.CONTENT_BOTTOM_OFFSET,
-        'ENABLE_CHAT_ROOMS': app_settings.ENABLE_CHAT_ROOMS,
+        "video": video,
     }
-    return render(request, 'videos/video_detail.html', context)
-
-
-
+    return render(request, "videos/video_detail.html", context)
